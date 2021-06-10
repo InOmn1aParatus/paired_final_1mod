337f97308d690ad9e10bb447de8af6bc5e735203
Matt Kragen <mattdkragen@gmail.com>
2021-06-10 09:03:10 -0700

    Add test for attributes

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,9 +3,17 @@ require './lib/passenger'
 
 RSpec.describe Passenger do
   context 'instantiation' do
-    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-    expect(charlie).to be_a(Passenger)
-    expect(taylor).to be_a(Passenger)
+    it 'exists' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      expect(charlie).to be_a(Passenger)
+      expect(taylor).to be_a(Passenger)
+    end
+
+    it 'has attributes' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      expect(charlie.name).to eq("Charlie")
+      expect(charlie.age).to eq(18)
+    end
   end
 end
