1d67aebb0743e3426c54c09428cd0b3962b30347
Matt Kragen <mattdkragen@gmail.com>
2021-06-10 11:41:05 -0700

    Add test for attributes

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Vehicle do
       vehicle = Vehicle.new("2001", "Honda", "Civic")
       expect(vehicle).to be_a(Vehicle)
     end
+
+    it 'has attributes' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      expect(vehicle.year).to eq('2001')
+      expect(vehicle.make).to eq('Honda')
+      expect(vehicle.model).to eq('Civic')
+    end
   end
 end
