d5296d9b7dc18ef38b3703251e233655adb9f5c8
Matt Kragen <mattdkragen@gmail.com>
2021-06-11 08:12:04 -0700

    Add test for adding cars

--- spec/drive_in_spec.rb
+++ spec/drive_in_spec.rb
@@ -20,4 +20,16 @@ RSpec.describe DriveIn do
       expect(drive_in.parked).to eq([])
     end
   end
+
+  context 'methods' do
+    it 'adds vehicles to the lot' do
+      vehicle1 = Vehicle.new('2001', 'Honda', 'Civic')
+      vehicle2 = Vehicle.new('2015', 'Hyundai', 'Sonata')
+      drive_in = DriveIn.new('Eldritch Drive-in')
+      drive_in.add_vehicle(vehicle1)
+      drive_in.add_vehicle(vehicle2)
+
+      expect(drive_in.parked).to eq([vehicle1, vehicle2])
+    end
+  end
 end
